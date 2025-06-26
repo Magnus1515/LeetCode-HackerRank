e7e632d0344920d4ceb31a5b84ec591d84c6c383
David Roldan <rdavideliseo15@gmail.com>
2025-06-25 22:56:37 -0700

    Submitting 3392 problem this one took me two days to do it

--- CountSubarraysofLengthThreeWithaCondition/src/main/java/org/example/Solution.java
+++ CountSubarraysofLengthThreeWithaCondition/src/main/java/org/example/Solution.java
@@ -1,25 +1,28 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Solution {
     public static int countSubarrays(int[] nums) {
         int result = 0;
-        for (int i = 0; i < nums.length; i++){
-            for(int j = 0; j < nums.length; j++){
-                for (int k = 0; k < nums.length; k++){
+        int s = 0;
+        int h = 2;
+        int n = nums.length;
 
-                    if (i == j || j == k || i ==k){
-                        continue;
-                    }
-                    if (i + k == j / 2 ) {
-                        result = 1;
-                        break;
-                    }else{
-                        result = 0;
-                    }
+        while (h < n) {
 
-                }
+            int[] tempArray = Arrays.copyOfRange(nums, s, h+1);
 
+            double firstPart = tempArray[0] + (tempArray[2]);
+            double secondPart = tempArray[1] * 0.5;
+//            System.out.println(firstPart + " " + secondPart);
+            if (firstPart == secondPart) {
+                result++;
+            } else if (firstPart != secondPart) {
+                result += 0;
             }
+            s ++;
+            h ++;
         }
         return result;
     }
